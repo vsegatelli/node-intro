93cd6df23727eed180f01dcc5999a34c9e236cdc
Victor <victorsegatelli@gmail.com>
2020-11-17 21:55:32 -0300

    passando JSON dessa vez

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    })
-    console.log('Servidor rodando corretamente!')
-  res.end('Servidor funcionando novamente Agora de um jeito mais correto!')
+  res.writeHead(200, {
+    'Content-Type': 'application ld/json'
+  })
+  console.log('Servidor rodando corretamente!')
+  res.end('[{"nome":"Fulano","sobrenome":"Silva"},{"nome":"Ciclano","sobrenome":"Silva"},{"nome":"Beltrano","sobrenome":"Silva"}]')
 }).listen(3030, 'localhost')
